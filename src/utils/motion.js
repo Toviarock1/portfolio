7d587c38f9232572681c5b285eec550aa18793bd
Toviarock1 <simontoviaadama@gmail.com>
2023-03-30 15:14:40 +0100

    fix: show works on small devices

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -16,6 +16,23 @@ export const textVariant = (delay) => {
   };
 };
 
+export const textVariant2 = (delay) => {
+  return {
+    hidden: {
+      y: -50,
+    },
+    show: {
+      y: 0,
+
+      transition: {
+        type: 'spring',
+        duration: 1.25,
+        delay: delay,
+      },
+    },
+  };
+};
+
 export const fadeIn = (direction, type, delay, duration) => {
   return {
     hidden: {
@@ -27,6 +44,26 @@ export const fadeIn = (direction, type, delay, duration) => {
       x: 0,
       y: 0,
       opacity: 1,
+      transition: {
+        type: type,
+        delay: delay,
+        duration: duration,
+        ease: 'easeOut',
+      },
+    },
+  };
+};
+
+export const fadeIn2 = (direction, type, delay, duration) => {
+  return {
+    hidden: {
+      x: direction === 'left' ? 100 : direction === 'right' ? -100 : 0,
+      y: direction === 'up' ? 100 : direction === 'down' ? -100 : 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+
       transition: {
         type: type,
         delay: delay,
